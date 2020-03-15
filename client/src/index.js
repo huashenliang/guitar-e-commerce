59d37a337e62288de92ec66b72ec1ec1bc2e013b
huashenliang <JohnnyLiang77@gmail.com>
2020-03-15 13:18:47 -0500

    Added redux store

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,22 @@ import {BrowserRouter} from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './Resources/css/style.css';
 
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+import Reducer from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
+
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
+    <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}> 
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+
+    </Provider>
+    
 ,document.getElementById('root'));
