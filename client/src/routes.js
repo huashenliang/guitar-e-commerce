2a5a612263137ce317bf9ce9c0c7960367f0f110
huashenliang <JohnnyLiang77@gmail.com>
2020-03-15 13:42:30 -0500

    created register route and button

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,12 +3,15 @@ import {Switch, Route} from 'react-router-dom';
 
 import Home from './Components/Home';
 import Layout from './hoc/layout';
+import RegisterLogin from './Components/Register_login';
+
 
 
 const Routes = () => {
     return(
         <Layout>
             <Switch>
+            <Route path="/register_login" exact component={RegisterLogin} />
             <Route path="/" exact component={Home} />
         </Switch>
         </Layout>
