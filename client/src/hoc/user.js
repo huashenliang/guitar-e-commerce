1f9b1ca1beb6e3e739b7936ed18d74d441e44d26
huashenliang <JohnnyLiang77@gmail.com>
2020-03-16 16:44:48 -0500

    creating the user dashboard

---
+++ client/src/hoc/user.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const links = [
+    {
+        name:  'My account',
+        linkTo:'/user/dashboard'
+    },
+    {
+        name:  'User information',
+        linkTo:'/user/user_profile'
+    },
+    {
+        name:  'My Cart',
+        linkTo:'/user/cart'
+    },
+]
+
+
+
+const UserLayout = (props) => {
+
+    return ( 
+        <div className="container">
+            <div className="user_container">
+                <div className="user_left_nav">
+                    <h2> My Account</h2>
+                    <div className="links">
+                        { links.map((item, i) => (
+                            <Link to={item.linkTo} key={i}>
+                                {item.name}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                <div className="user_right_nav">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default UserLayout;
--- client/src/routes.js
+++ client/src/routes.js
@@ -5,16 +5,17 @@ import Home from './Components/Home';
 import Layout from './hoc/layout';
 import RegisterLogin from './Components/Register_login';
 import Register from './Components/Register_login/register';
-
+import UserDashBoard from './Components/User';
 
 const Routes = () => {
     return(
         <Layout>
             <Switch>
-            <Route path="/register" exact component={Register} />
-            <Route path="/register_login" exact component={RegisterLogin} />
-            <Route path="/" exact component={Home} />
-        </Switch>
+                 <Route path="/user/dashboard" exact component={UserDashBoard} />
+                <Route path="/register" exact component={Register} />
+                <Route path="/register_login" exact component={RegisterLogin} />
+                <Route path="/" exact component={Home} />
+            </Switch>
         </Layout>
         
     )
