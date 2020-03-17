7425f48777c3811c1843f35bdf5493c78fda0e53
huashenliang <JohnnyLiang77@gmail.com>
2020-03-17 13:07:12 -0500

    adding sliders in home page

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import HomeSlider from './home_slider';
 class Home extends Component {
     state = {  }
     render() { 
         return ( 
             <div>
-                Home
+                <HomeSlider />
             </div>
          );
     }
